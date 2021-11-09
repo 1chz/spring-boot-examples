5ad9cf549501f24ef0d75b9dc0c4a9c459c98d65
shirohoo <shirohoo4102@gmail.com>
2021-11-09 16:25:05 +0900

    :sparkles: 순서를 보장하는 어드바이저 예제코드 작성

---
+++ spring-aop/src/main/java/io/github/shirohoo/springaop/advisor/OrderedAdvisor.java
@@ -0,0 +1,36 @@
+package io.github.shirohoo.springaop.advisor;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
+
+// 순서를 보장하는 어드바이저
+// 기본적으로 @Aspect는 순서를 보장하지 않는다
+// @Order를 사용하면 순서를 보장할 수 있으나 메서드 단위에는 적용이 되지 않는다
+// 따라서 포인트컷 1, 어드바이스 1로 이루어진 어드바이저 클래스를 따로 만들고 @Order를 작성해야 한다
+public class OrderedAdvisor {
+
+    @Aspect
+    @Order(1) // 가장 처음 실행될 어드바이저
+    public static class doSomethingAdvisor1 {
+
+        @Around("io.github.shirohoo.springaop.pointcut.Pointcuts.controllers()")
+        public Object doSomething(ProceedingJoinPoint joinPoint) throws Throwable {
+            return joinPoint.proceed();
+        }
+
+    }
+
+    @Aspect
+    @Order(2) // 두번째로 실행될 어드바이저
+    public static class doSomethingAdvisor2 {
+
+        @Around("io.github.shirohoo.springaop.pointcut.Pointcuts.services()")
+        public Object doSomething(ProceedingJoinPoint joinPoint) throws Throwable {
+            return joinPoint.proceed();
+        }
+
+    }
+
+}
