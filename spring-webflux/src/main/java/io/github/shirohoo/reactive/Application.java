cea7b3c49dc8c921933968a6f59bc96c553669db
Changhun Han <shirohoo4102@gmail.com>
2022-01-09 15:08:47 +0900

    ✨  웹플럭스 3장 완료 (#22)
    * :sparkles: devtools 추가
    
    * :sparkles: logging.level.web=debug
    
    * :sparkles: blockhound 추가
    
    - 블로킹 API가 호출되면 감지하여 알려준다.
    
    * :sparkles: blockhound에 블로킹 API 허용하는 코드 작성
    
    blockhound가 감지한 블로킹 API
    - TemplateEngine.process()
    - FileInputStream.readBytes()
    
    * :memo: README.md 작성

--- spring-webflux/src/main/java/io/github/shirohoo/reactive/Application.java
+++ spring-webflux/src/main/java/io/github/shirohoo/reactive/Application.java
@@ -1,11 +1,19 @@
 package io.github.shirohoo.reactive;
 
+import java.io.FileInputStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.thymeleaf.TemplateEngine;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        BlockHound.builder()
+            .allowBlockingCallsInside(TemplateEngine.class.getCanonicalName(), "process")
+            .allowBlockingCallsInside(FileInputStream.class.getCanonicalName(), "readBytes")
+            .install();
+
         SpringApplication.run(Application.class, args);
     }
 }
