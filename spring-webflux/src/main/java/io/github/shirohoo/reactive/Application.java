db2c3b8b6b9223018ada643041c235f60ab66e4c
shirohoo <shirohoo4102@gmail.com>
2022-01-09 14:53:55 +0900

    :sparkles: blockhound 추가
    - 블로킹 API가 호출되면 감지하여 알려준다.

--- spring-webflux/src/main/java/io/github/shirohoo/reactive/Application.java
+++ spring-webflux/src/main/java/io/github/shirohoo/reactive/Application.java
@@ -2,10 +2,12 @@ package io.github.shirohoo.reactive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        BlockHound.install();
         SpringApplication.run(Application.class, args);
     }
 }
